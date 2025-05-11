1ffdd26e21ee8fb09df96193cc081cc0252d216c
Sparsh Soni <sparsh.officialwork@gmail.com>
2025-05-11 19:54:41 +0530

    feat(db): Initialize the db schema

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+# we define our data entities, blueprints for dataobjects , we are going to work with
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField()
