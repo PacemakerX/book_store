8fab9e32a6822ee5d63b6370b545acb23be335ae
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-13 16:35:29 +0530

    feat(book_outlet): Add url paths

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Book
 # Create your views here.
+
+def all_book_details(request):
+    books=Book.objects.all()
+    return render(request,'./book_outlet/index.html',{
+        "books": books
+    })
