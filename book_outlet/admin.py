f1d8bc2c965e62a205267039ded004635a858d16
Sparsh Soni <sparsh.officialwork@gmail.com>
2025-05-13 21:20:14 +0530

    feat(admin): Add admin config

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Book
 # Register your models here.
-admin.site.register(Book)
+
+
+class BookAdmin(admin.ModelAdmin):
+    # prepopulated_fields = {"slug": ("title",)}
+    list_filter=('author','title','rating')
+    list_display=('title','rating')
+admin.site.register(Book, BookAdmin)
