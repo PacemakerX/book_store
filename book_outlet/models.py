784c173cc565e7a8425c58947844335ffa028fdd
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-13 20:37:38 +0530

    feat(db): Add a new field to db

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -12,11 +12,18 @@ class Book(models.Model):
     )
     author = models.CharField(max_length=100)
     is_bestselling = models.BooleanField()
+    slug = models.SlugField( blank=True)
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self.title.replace(" ", "-").lower()
+        super().save(*args, **kwargs)
         
     def __str__(self):
         return (
             f"Name: {self.title}\n"
             f"Rating: {self.rating}\n"
             f"Author: {self.author}\n"
-            f"Is Bestselling: {'YES' if self.is_bestselling else 'NO'}"
+            f"Is Bestselling: {'YES' if self.is_bestselling else 'NO'}\n"
+            f"Slug: {self.slug}"
         )
