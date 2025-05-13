028e0fbe9dc9847869d1db6266511bc51c6b027b
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-13 17:37:59 +0530

    fix(book_outlet): Add a function to open a particular book detail

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -7,3 +7,13 @@ def all_book_details(request):
     return render(request,'./book_outlet/index.html',{
         "books": books
     })
+    
+def book_details(request, book_title):
+    book_title = " ".join(word.capitalize() for word in book_title.split("-"))
+    try:
+        book = Book.objects.get(title=book_title)
+    except Book.DoesNotExist:
+        book = None
+    return render(request, './book_outlet/book_detail.html', {
+        'book': book
+    })
