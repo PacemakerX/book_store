a04c4b343afb2e010fd6045622f179deb331078b
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-13 13:35:32 +0530

    feat(db): Add a better print statement

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -8,10 +8,15 @@ from django.core.validators import MinValueValidator,MaxValueValidator
 class Book(models.Model):
     title = models.CharField(max_length=50)
     rating = models.IntegerField(
-        validators=[MinValueValidator(1),MaxValueValidator(10)]
+        validators=[MinValueValidator(1), MaxValueValidator(10)]
     )
-    author= models.CharField(max_length=100)
-    is_bestselling=models.BooleanField()
+    author = models.CharField(max_length=100)
+    is_bestselling = models.BooleanField()
         
     def __str__(self):
-        return f"Name: {self.title} Raiting:{self.rating} Is_BestSelling:{'YES' if self.is_bestselling else 'NO'}"
+        return (
+            f"Name: {self.title}\n"
+            f"Rating: {self.rating}\n"
+            f"Author: {self.author}\n"
+            f"Is Bestselling: {'YES' if self.is_bestselling else 'NO'}"
+        )
