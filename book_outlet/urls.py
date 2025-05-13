ba7a8fa9113e5879c38819f23050d0c22cf3ff5e
PacemakerX <sparsh.officialwork@gmail.com>
2025-05-13 17:37:28 +0530

    fix(book_outlet): update url to open a particular book detail

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.all_book_details,name='all_book_details'),
-    # path("")
+    path("", views.all_book_details, name='all_book_details'),
+    path("<str:book_title>", views.book_details, name='book_details')
 ]
